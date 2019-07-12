ce1aa2f488892445cb9e4022687c807d808fec52
hawa <yesiha2001@gmail.com>
2019-07-13 02:04:27 +0400

    this

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 import express from 'express';
-import apiRouters from './app/routers/apiRouters'
-import swaggerUi from 'swagger-ui-express'
-import swaggerDoc from './swagger.json'
 
 const app = express()
 const port = 3000
@@ -9,8 +6,10 @@ const port = 3000
 app.use(express.json());
 
 
-app.use('/api/v1/', apiRouters)
-app.use('/doc',swaggerUi.serve,swaggerUi.setup(swaggerDoc));
+app.get('/', (req,res)=>{
+res.send('hi')
+})
+
 
 // where page not found
 app.use((req, res, next) => {
@@ -26,5 +25,3 @@ app.use((req, res, next) => {
 
 
 app.listen(port, () => console.log(`listening on port ${port}...!`));
-
-export default app;
